d1b5404a5eb43b3ae4a33e6c52f6a6ad698a338f
dexter9006 <52006237+dexter9006@users.noreply.github.com>
2022-12-12 18:55:00 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,7 +5,7 @@ Config = {}
 
 Config.DevMode = false -- DIABLE THIS IF YOUR SERVER IS LIVE
 
-Config.Linux = false -- If they use a Linux server, then true
+Config.Linux = true -- If they use a Linux server, then true
 
 Config.butcherfunction = true -- set to true if you want to use butcher functions
 ----------------------------- TRANSLATE HERE -------------------------------------
@@ -14,9 +14,9 @@ Config.Language = {
     NotInTheButcher = "I won't buy that animal",
     AnimalSold = "Thanks for the deal you got ",
     SkinnableAnimalstowed = "You obtained ",
-    stow = "Stow",
-    press = "Press",
-    sell = "Sell",
+    stow = "Accrocher",
+    press = "Appuyer sur",
+    sell = "Vendre",
     notabutcher = "You Dont Have The Right Job",
     dollar = " $",
     join = " and ",
@@ -33,7 +33,7 @@ Config.aiButcherped = true -- spawn ai butched ped set to false if you dont want
 
 Config.joblocked = false -- lock the butcher so only people with the job can access. u can change access to each butcher by editing this  butcherjob = "butcher"
 
-Config.maxpelts = 2 -- max pelts allowed on back of horse (Max 3 allowed)
+Config.maxpelts = 3 -- max pelts allowed on back of horse
 
 ------------------- Item Quantity Instructions -----------------
 -- The range for when a skinnableAnimal or Animal has a config value givenAmount of 0.
@@ -45,16 +45,16 @@ Config.ItemQuantity = {
 
 
 Config.Butchers = {
-    { butchername = "Valentine Butcher",   butcherjob = "butcher", blip = 1369919445, npcmodel = "S_M_M_UNIBUTCHERS_01", coords = vector3(-339.0141, 767.6358, 115.5645),   heading = 100.41544342041,  radius = 3.0, showblip = true },
-    { butchername = "Strawberry Butcher",  butcherjob = "butcher", blip = 1369919445, npcmodel = "S_M_M_UNIBUTCHERS_01", coords = vector3(-1753.143, -392.4201, 155.2578),  heading = 181.37438964844,  radius = 3.0, showblip = true },
-    { butchername = "Blackwater Butcher",  butcherjob = "butcher", blip = 1369919445, npcmodel = "S_M_M_UNIBUTCHERS_01", coords = vector3(-754.0, -1285.158, 43.03),        heading = 273.579,          radius = 3.0, showblip = true },
-    { butchername = "Annesburg Butcher",   butcherjob = "butcher", blip = 1369919445, npcmodel = "S_M_M_UNIBUTCHERS_01", coords = vector3(2934.51, 1301.159, 43.48365),     heading = 70.572128295898,  radius = 3.0, showblip = true },
-    { butchername = "Van Horn Butcher",    butcherjob = "butcher", blip = 1369919445, npcmodel = "S_M_M_UNIBUTCHERS_01", coords = vector3(2991.844, 572.0218, 43.36182),    heading = 259.52850341797,  radius = 3.0, showblip = true },
-    { butchername = "Rhodes Butcher",      butcherjob = "butcher", blip = 1369919445, npcmodel = "S_M_M_UNIBUTCHERS_01", coords = vector3(1297.578, -1277.589, 74.88102),   heading = 146.60472106934,  radius = 3.0, showblip = true },
-    { butchername = "Armadillo Butcher",   butcherjob = "butcher", blip = 1369919445, npcmodel = "S_M_M_UNIBUTCHERS_01", coords = vector3(-3691.438, -2623.152, -14.75121), heading = 0.46632757782936, radius = 3.0, showblip = true },
-    { butchername = "Tumbleweed Butcher",  butcherjob = "butcher", blip = 1369919445, npcmodel = "S_M_M_UNIBUTCHERS_01", coords = vector3(-5510.371, -2947.005, -1.894515), heading = 251.54911804199,  radius = 3.0, showblip = true },
-    { butchername = "Landing Butcher",     butcherjob = "butcher", blip = 1369919445, npcmodel = "CS_MP_SHAKY",          coords = vector3(-1435.61, -2330.28, 43.66),       heading = 1.15,             radius = 4.0, showblip = true },
-    { butchername = "Saint Denis Butcher", butcherjob = "butcher", blip = 1369919445, npcmodel = "S_M_M_UNIBUTCHERS_01", coords = vector3(2819.54, -1331.21, 45.00),        heading = 51.8221321532,    radius = 4.0, showblip = true }
+    { butchername = "Boucher de Valentine",   butcherjob = "butcher", blip = 1369919445, npcmodel = "S_M_M_UNIBUTCHERS_01", coords = vector3(-339.0141, 767.6358, 115.5645),   heading = 100.41544342041,  radius = 3.0, showblip = true },
+    { butchername = "Boucher de Strawberry",  butcherjob = "butcher", blip = 1369919445, npcmodel = "S_M_M_UNIBUTCHERS_01", coords = vector3(-1753.143, -392.4201, 155.2578),  heading = 181.37438964844,  radius = 3.0, showblip = true },
+    { butchername = "Boucher de Blackwater",  butcherjob = "butcher", blip = 1369919445, npcmodel = "S_M_M_UNIBUTCHERS_01", coords = vector3(-754.0, -1285.158, 43.03),        heading = 273.579,          radius = 3.0, showblip = true },
+    { butchername = "Boucher d'Annesburg",   butcherjob = "butcher", blip = 1369919445, npcmodel = "S_M_M_UNIBUTCHERS_01", coords = vector3(2934.51, 1301.159, 43.48365),     heading = 70.572128295898,  radius = 3.0, showblip = true },
+    { butchername = "Boucher de Van Horn",    butcherjob = "butcher", blip = 1369919445, npcmodel = "S_M_M_UNIBUTCHERS_01", coords = vector3(2991.844, 572.0218, 43.36182),    heading = 259.52850341797,  radius = 3.0, showblip = true },
+    { butchername = "Boucher de Rhodes",      butcherjob = "butcher", blip = 1369919445, npcmodel = "S_M_M_UNIBUTCHERS_01", coords = vector3(1297.578, -1277.589, 74.88102),   heading = 146.60472106934,  radius = 3.0, showblip = true },
+    { butchername = "Boucher d'Armadillo",   butcherjob = "butcher", blip = 1369919445, npcmodel = "S_M_M_UNIBUTCHERS_01", coords = vector3(-3691.438, -2623.152, -14.75121), heading = 0.46632757782936, radius = 3.0, showblip = true },
+    { butchername = "Boucher de Tumbleweed",  butcherjob = "butcher", blip = 1369919445, npcmodel = "S_M_M_UNIBUTCHERS_01", coords = vector3(-5510.371, -2947.005, -1.894515), heading = 251.54911804199,  radius = 3.0, showblip = true },
+    { butchername = "Boucher de Landing",     butcherjob = "butcher", blip = 1369919445, npcmodel = "CS_MP_SHAKY",          coords = vector3(-1435.61, -2330.28, 43.66),       heading = 1.15,             radius = 4.0, showblip = true },
+    { butchername = "Boucher de Saint Denis", butcherjob = "butcher", blip = 1369919445, npcmodel = "S_M_M_UNIBUTCHERS_01", coords = vector3(2819.54, -1331.21, 45.00),        heading = 51.8221321532,    radius = 4.0, showblip = true }
 }
 
 -----------------ANIMAL INSTRUCTIONS  -----------------
@@ -74,25 +74,25 @@ Config.SkinnableAnimals = {
     --small animals skin them and sell them at the butcher no need to add them to  CONFIG.ANIMALS
     [989669666]   = { name = "Rat", givenItem = { "meat" }, givenAmount = { 1 }, givenDisplay = {}, money = 100, gold = 0, rolPoints = 0, xp = 1 },
     [1458540991]  = { name = "Raccoon", givenItem = { "pelt" }, givenAmount = { 2 }, givenDisplay = {}, money = 100, gold = 0, rolPoints = 0, xp = 1 },
-    [-1003616053] = { name = "Duck", givenItem = { "feathers" }, givenAmount = { 0 }, givenDisplay = {}, money = 100, gold = 0, rolPoints = 0, xp = 1 },
+    [-1003616053] = { name = "Canard", givenItem = { "feathers" }, givenAmount = { 0 }, givenDisplay = {}, money = 100, gold = 0, rolPoints = 0, xp = 1 },
 
     -- big animals when you skin them it gives you items. if you have it added TO CONFIG.ANIMALS JUST COPY PASTE HERE AND REMOVE THE GIVE MONEY ETC LIKE SHOWN BELOW.
     [1110710183]  = { name = "Deer", givenItem = { "meat" }, givenAmount = { 0 }, givenDisplay = {}, money = 0, gold = 0, rolPoints = 0, xp = 1 },
-    [-1124266369] = { name = "Bear", givenItem = { "bearHeart", "bearClaws" }, givenAmount = { 1, 3 }, givenDisplay = { "Heart", "Claw"}, money = 0, gold = 0, rolPoints = 0, xp = 1 },
+    [-1124266369] = { name = "Ours", givenItem = { "bearHeart", "bearClaws" }, givenAmount = { 1, 3 }, givenDisplay = { "Heart", "Claw"}, money = 0, gold = 0, rolPoints = 0, xp = 1 },
 }
 
 -- Animals that are traded in to the butcher
 Config.Animals = {
     -- Animals
-    [-1124266369]  = { name = "Bear", givenItem = { "meat" }, givenAmount = { 0 }, money = 100, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = 957520252, good = 143941906, perfect = 1292673537 },
+    [-1124266369]  = { name = "Ours", givenItem = { "meat" }, givenAmount = { 0 }, money = 100, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = 957520252, good = 143941906, perfect = 1292673537 },
     [-15687816381] = { name = "Big Horn Ram", givenItem = { "meat" }, givenAmount = { 0 }, money = 50, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = 1796037447, good = -476045512, perfect = 1795984405 },
     [2028722809]   = { name = "Boar", givenItem = { "meat" }, givenAmount = { 0 }, money = 4, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = 1248540072, good = nil, perfect = -1858513856 },
     [-1963605336]  = { name = "Buck", givenItem = { "meat" }, givenAmount = { 0 }, money = 7, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = 1603936352, good = -868657362, perfect = -702790226 },
     [1556473961]   = { name = "Bison", givenItem = { "meat" }, givenAmount = { 0 }, money = 40, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = -1730060063, good = -591117838, perfect = -237756948 },
     [1957001316]   = { name = "Bull", givenItem = { "meat" }, givenAmount = { 0 }, money = 40, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = 9293261, good = -336086818, perfect = -53270317 },
     [1110710183]   = { name = "Deer", givenItem = { "meat" }, givenAmount = { 0 }, money = 10, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = -662178186, good = -1827027577, perfect = -1035515486 },
-    [-1003616053]  = { name = "Duck", givenItem = { "feathers" }, givenAmount = { 0 }, money = 3, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
-    [1459778951]   = { name = "Eagle", givenItem = { "feathers" }, givenAmount = { 0 }, money = 5, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
+    [-1003616053]  = { name = "Canard", givenItem = { "feathers" }, givenAmount = { 0 }, money = 3, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
+    [1459778951]   = { name = "Aigle", givenItem = { "feathers" }, givenAmount = { 0 }, money = 5, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
     [831859211]    = { name = "Egret", givenItem = { "feathers" }, givenAmount = { 0 }, money = 3, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
     [-2021043433]  = { name = "Elk", givenItem = { "meat" }, givenAmount = { 0 }, money = 10, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = 2053771712, good = 1181652728, perfect = -1332163079 },
     [252669332]    = { name = "American Red Fox", givenItem = { "meat" }, givenAmount = { 0 }, money = 3, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = 1647012424, good = 238733925, perfect = 500722008 },
@@ -101,9 +101,9 @@ Config.Animals = {
     [-829273561]   = { name = "Small Grey Wolf", givenItem = { "meat" }, givenAmount = { 0 }, money = 6, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = 85441452, good = 1145777975, perfect = 653400939 },
     [1104697660]   = { name = "Vulture", givenItem = { "feathers" }, givenAmount = { 0 }, money = 3, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
     [-407730502]   = { name = "Snapping Turtle", givenItem = { "meat" }, givenAmount = { 0 }, money = 3, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
-    [-466054788]   = { name = "Wild Turkey", givenItem = { "feathers" }, givenAmount = { 0 }, money = 3, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
-    [-2011226991]  = { name = "Wild Turkey", givenItem = { "feathers" }, givenAmount = { 0 }, money = 3, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
-    [-166054593]   = { name = "Wild Turkey", givenItem = { "feathers" }, givenAmount = { 0 }, money = 3, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
+    [-466054788]   = { name = "Dinde Sauvage", givenItem = { "feathers" }, givenAmount = { 0 }, money = 3, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
+    [-2011226991]  = { name = "Dinde Sauvage", givenItem = { "feathers" }, givenAmount = { 0 }, money = 3, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
+    [-166054593]   = { name = "Dinde Sauvage", givenItem = { "feathers" }, givenAmount = { 0 }, money = 3, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
     [-229688157]   = { name = "Water Snake", givenItem = { "meat" }, givenAmount = { 0 }, money = 2, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
     [-1790499186]  = { name = "Snake Red Boa", givenItem = { "meat" }, givenAmount = { 0 }, money = 2, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
     [1464167925]   = { name = "Snake Fer-De-Lance", givenItem = { "meat" }, givenAmount = { 0 }, money = 2, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
@@ -157,14 +157,14 @@ Config.Animals = {
     [1538187374]   = { name = "Channel Catfish", givenItem = { "fish" }, givenAmount = { 0 }, money = 3, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
     [697075200]    = { name = "Northern Pike", givenItem = { "fish" }, givenAmount = { 0 }, money = 2, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
     [1867262572]   = { name = "Bluegill", givenItem = { "fish" }, givenAmount = { 0 }, money = 2, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
-    [1493541632]   = { name = "Bullhead catfish", givenItem = { "fish" }, givenAmount = { 0 }, money = 2, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
+    [1493541632]   = { name = "Bullhead Catfish", givenItem = { "fish" }, givenAmount = { 0 }, money = 2, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
     [3111984125]   = { name = "Chain Pickerl", givenItem = { "fish" }, givenAmount = { 0 }, money = 2, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
-    [463643368]    = { name = "bigmouth bass", givenItem = { "fish" }, givenAmount = { 0 }, money = 2, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
+    [463643368]    = { name = "bigmouth Bass", givenItem = { "fish" }, givenAmount = { 0 }, money = 2, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
     [3842742512]   = { name = "Perch", givenItem = { "fish" }, givenAmount = { 0 }, money = 2, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
-    [134747314]    = { name = "Rainbow trout", givenItem = { "fish" }, givenAmount = { 0 }, money = 2, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
+    [134747314]    = { name = "Rainbow Trout", givenItem = { "fish" }, givenAmount = { 0 }, money = 2, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
     [4051778898]   = { name = "Redfin Pickerl", givenItem = { "fish" }, givenAmount = { 0 }, money = 2, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
     [2313405824]   = { name = "Rock Bass", givenItem = { "fish" }, givenAmount = { 0 }, money = 2, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
-    [2410477101]   = { name = "Smallmouth bass", givenItem = { "fish" }, givenAmount = { 0 }, money = 2, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
+    [2410477101]   = { name = "Smallmouth Bass", givenItem = { "fish" }, givenAmount = { 0 }, money = 2, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
     [543892122]    = { name = "Salmon Redfish", givenItem = { "fish" }, givenAmount = { 0 }, money = 2, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
-    [1702636991]   = { name = "Salmon sockeye", givenItem = { "fish" }, givenAmount = { 0 }, money = 2, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
+    [1702636991]   = { name = "Salmon Sockeye", givenItem = { "fish" }, givenAmount = { 0 }, money = 2, gold = 0, rolPoints = 0, xp = 3, poorQualityMultiplier = 1.0, goodQualityMultiplier = 1.5, perfectQualityMultiplier = 2, poor = nil, good = nil, perfect = nil },
 }
